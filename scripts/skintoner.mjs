72397d08898844d25ffae8e4757b84dcfa966984
Gabe616 <poggerschampersqwq@gmail.com>
2023-08-26 19:39:28 +0200

    ok

--- scripts/skintoner.mjs
+++ scripts/skintoner.mjs
@@ -5,12 +5,12 @@ for (const category of await readdir("Emojis")) {
   if (category.endsWith(".mjs")) continue;
   for (const emoji of await readdir(join("Emojis", category))) {
     const nfile = (emoji[0].toUpperCase() + emoji.slice(1).toLowerCase())
-      .replace(/dark/, "Dark")
-      .replace(/default/, "Default")
-      .replace(/light/, "Light")
-      .replace(/medium/, "Medium")
-      .replace(/medium-dark/, "Medium-Dark")
-      .replace(/medium-light/, "Medium-Light");
+      .replace(/\bdark\b/, "Dark")
+      .replace(/\bdefault\b/, "Default")
+      .replace(/\blight\b/, "Light")
+      .replace(/\bmedium\b/, "Medium")
+      .replace(/\bmedium-dark\b/, "Medium-Dark")
+      .replace(/\bmedium-light\b/, "Medium-Light");
     await rename(
       join("Emojis", category, emoji),
       join("Emojis", category, nfile)
